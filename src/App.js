9f23ce109a1581043edb0b28b0f91dcafbc603d1
Roopa1 <Roopa1@nextgen.com>
2024-02-03 22:39:25 +0530

    adding weather cards

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ function App() {
     const fetchData = async () =>{
       try{
         const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=f6ef39f9a1d94b98943165233232709&q=${searchCity}`)
-        // if (!res.ok) {
-        //   throw new Error('Failed to fetch weather data');
-        // }
         const data = await res.json();
         setCityData(data);
         setIsLoading(false);
@@ -26,33 +23,34 @@ function App() {
     fetchData();    
   }
 
-
   return (
     <div>
       <div className='wrapper'>
-      <input type='text' className='inpt' placeholder='Enter city name' value={searchCity} onChange={(e)=>{setSearchCity(e.target.value)}}/>
-      <button className='btn' onClick={handleSearch}>Search</button>
+        <input type='text' className='inpt' placeholder='Enter city name' value={searchCity} onChange={(e)=>{setSearchCity(e.target.value)}}/>
+        <button className='btn' onClick={handleSearch}>Search</button>
       </div>
-      {isLoading?(<p className='cardWrapper'>Loading Data...</p>):cityData && ( <div className='cardWrapper'>
-        <div className='weather-card'>
-          <h4>Temperature</h4>
-          <div>{cityData.current.temp_c}°C</div>
+      {isLoading ? (
+        <p className='cardWrapper'>Loading Data...</p>
+      ) : cityData && (
+        <div className='cardWrapper weather-cards'> {/* Added 'weather-cards' class here */}
+          <div className='weather-card'>
+            <h4>Temperature</h4>
+            <div>{cityData.current.temp_c}°C</div>
+          </div>
+          <div className='weather-card'>
+            <h4>Humidity</h4>
+            <div>{cityData.current.humidity}%</div>
+          </div>
+          <div className='weather-card'>
+            <h4>Condition</h4>
+            <div>{cityData.current.condition.text}</div>
+          </div>
+          <div className='weather-card'>
+            <h4>Wind Speed</h4>
+            <div>{cityData.current.wind_kph} kph</div>
+          </div>
         </div>
-        <div className='weather-card'>
-          <h4>Humidity</h4>
-          <div>{cityData.current.humidity}%</div>
-        </div>
-        <div className='weather-card'>
-          <h4>Condition</h4>
-          <div>{cityData.current.condition.text}</div>
-        </div>
-        <div className='weather-card'>
-          <h4>Wind Speed</h4>
-          <div>{cityData.current.wind_kph} kph</div>
-        </div>
-      </div>)}
-      
-     
+      )}
     </div>
   );
 }
